3098d7ebf01ab88a331518d55f11b2a98c9554cd
touchtak <159424108+touchtak@users.noreply.github.com>
2024-03-06 07:23:42 +0000

    delete追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,47 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
